8b8d9a018cbe7d7aa7f53613b800d8d98e307947
Robert Oostenveld <r.oostenveld@gmail.com>
2021-02-16 16:21:59 +0100

    updated the main script

--- make_headcast.m
+++ make_headcast.m
@@ -1,26 +1,46 @@
+%% this is only done once and results in a number of mat files with the corresponding objects
+
+% prepare_dewar
+% prepare_headcoils
+% prepare_combined_axes
+% prepare_vertex_cylinder
+% prepare_binoculars
 
 %% First phase: construct scalp surface from MRI
 
 %% - importing the DICOM images
 
-% dicomfile = '../raw/ses-001/007-gre_headcast_largeFOV_T1w_WaterExcite/PILOT_HAGGORT.MR.KRIARM_SKYRA.0007.0001.2019.04.11.09.36.04.649721.547271769.IMA';
-dicomfile = '../raw/ses-001/008-gre_headcast_largeFOV_T1w_WaterExcite/PILOT_HAGGORT.MR.KRIARM_SKYRA.0008.0001.2019.04.11.09.36.04.649721.547273926.IMA';
-% dicomfile = '../raw/ses-001/009-gre_headcast_largeFOV_T1w_DistortCorrect_ND/PILOT_HAGGORT.MR.KRIARM_SKYRA.0009.0001.2019.04.11.09.36.04.649721.547288155.IMA';
-% dicomfile = '../raw/ses-001/010-gre_headcast_largeFOV_T1w_DistortCorrect/PILOT_HAGGORT.MR.KRIARM_SKYRA.0010.0001.2019.04.11.09.36.04.649721.547291918.IMA';
+% dicomfile = '../sub-jm/ses-001/007-gre_headcast_largeFOV_T1w_WaterExcite/PILOT_HAGGORT.MR.KRIARM_SKYRA.0007.0001.2019.04.11.09.36.04.649721.547271769.IMA';
+% dicomfile = '../sub-jm/ses-001/008-gre_headcast_largeFOV_T1w_WaterExcite/PILOT_HAGGORT.MR.KRIARM_SKYRA.0008.0001.2019.04.11.09.36.04.649721.547273926.IMA';
+% dicomfile = '../sub-jm/ses-001/009-gre_headcast_largeFOV_T1w_DistortCorrect_ND/PILOT_HAGGORT.MR.KRIARM_SKYRA.0009.0001.2019.04.11.09.36.04.649721.547288155.IMA';
+% dicomfile = '../sub-jm/ses-001/010-gre_headcast_largeFOV_T1w_DistortCorrect/PILOT_HAGGORT.MR.KRIARM_SKYRA.0010.0001.2019.04.11.09.36.04.649721.547291918.IMA';
+dicomfile = '../sub-bb/gre_headcast_largeFOV_T1w_DistortCorrect.nii';
+% dicomfile = '../sub-bb/gre_headcast_largeFOV_T1w_DistortCorrect_ND.nii'
+% dicomfile = '../sub-bb/t1_mprage_sag_ipat2_1p0iso.nii'
+
 
 mri_orig = ft_read_mri(dicomfile);
 
 cfg = [];
+cfg.location = 'center';
 ft_sourceplot(cfg, mri_orig);
 
 %% Do a coarse alignment of the MRI to the CTF coordinate system
 
-% Plotting the MRI slices allows for checking for left-right flips: there should be a vitamine E capsule visible at the right ear.
-% The coarse alignment is also needed for the segmentation of the scalp surface.
+% Plotting the MRI slices allows for checking for left-right flips: there should be a
+% vitamine E capsule visible at the right ear. The coarse alignment is also needed
+% for the segmentation of the scalp surface.
+
+% HERE AN INTERACTION IS REQUIRED IN FT_VOLUMEREALIGN
 
 cfg = [];
 cfg.coordsys = 'ctf';
 mri_coreg1 = ft_volumerealign(cfg, mri_orig);
+
+% flip it to get an approximate alignment of the ijk–axes to the xyz-axes
+cfg = [];
+cfg.method = 'flip';
+mri_coreg1 = ft_volumereslice(cfg, mri_coreg1);
 
 % Here you should check that the axes are pointing in the correct direction according to the CTF coordinate system with
 % +x to the nose,
@@ -28,6 +48,8 @@ mri_coreg1 = ft_volumerealign(cfg, mri_orig);
 % -y to the left.
 
 ft_determine_coordsys(mri_coreg1, 'interactive', false);
+
+save mri_coreg1 mri_coreg1
 
 %% - segment the scalp surface
 
@@ -37,9 +59,18 @@ cfg.spmversion = 'spm12';
 cfg.spmmethod = 'new';
 seg_coreg1 = ft_volumesegment(cfg, mri_coreg1);
 
+% fill the ears
+before = sum(seg_coreg1.scalp(:)) * abs(det(seg_coreg1.transform(1:3,1:3)));
+seg_coreg1.scalp = imclose(seg_coreg1.scalp, strel('sphere',6));
+after = sum(seg_coreg1.scalp(:)) * abs(det(seg_coreg1.transform(1:3,1:3)));
+fprintf('added %d ml around the ears\n', round((after-before)/1000)); 
+
 cfg = [];
 cfg.funparameter = 'scalp';
+cfg.location = 'center';
 ft_sourceplot(cfg, seg_coreg1, mri_coreg1);
+
+save seg_coreg1 seg_coreg1
 
 %% - construct the scalp surface
 
@@ -53,13 +84,13 @@ ft_plot_mesh(scalp_coreg1);
 ft_plot_axes(scalp_coreg1);
 camlight
 
-save mri_coreg1 mri_coreg1
-save seg_coreg1 seg_coreg1
 save scalp_coreg1 scalp_coreg1
 
 %% second phase: coregistration with the MEG coordinates
 
 %% - shift the scalp surface so that it is nicely aligned relative to the axes
+
+% HERE AN INTERACTION IS REQUIRED IN FT_INTERACTIVEREALIGN
 
 cfg = [];
 cfg.template.axes = 'yes';
@@ -70,8 +101,8 @@ transform = cfg.m;
 
 %% - apply the transformation from step 6 also to the anatomical MRI
 
-mri_coreg2  = ft_transform_geometry(transform, mri_coreg1);
-seg_coreg2  = ft_transform_geometry(transform, seg_coreg1);
+mri_coreg2   = ft_transform_geometry(transform, mri_coreg1);
+seg_coreg2   = ft_transform_geometry(transform, seg_coreg1);
 scalp_coreg2 = ft_transform_geometry(transform, scalp_coreg1);
 
 figure
@@ -79,8 +110,8 @@ ft_plot_mesh(scalp_coreg2);
 ft_plot_axes(scalp_coreg2);
 camlight
 
-save mri_coreg2 mri_coreg2
-save seg_coreg2 seg_coreg2
+save mri_coreg2   mri_coreg2
+save seg_coreg2   seg_coreg2
 save scalp_coreg2 scalp_coreg2
 
 %% - move the head localizer coils along the axes to the scalp surface
@@ -182,7 +213,7 @@ save headcoil_rpa_coreg3 headcoil_rpa_coreg3
 
 %% - export the aligned head surface as an STL file
 
-% flip the triangles inside-out, otherwise the surface is all dark in MeshLab
+% flip the triangles inside-out, otherwise the surface cannot be colored in MeshLab
 % this is based on trial and error
 scalp_coreg3.tri = fliplr(scalp_coreg3.tri);
 
@@ -205,38 +236,50 @@ ft_write_headshape('combined_axes.stl', combined_axes, 'format', 'stl');
 load vertex_cylinder
 ft_write_headshape('vertex_cylinder.stl', vertex_cylinder, 'format', 'stl');
 
+load dewar
+ft_write_headshape('dewar.stl', dewar, 'format', 'stl');
+
+load earflap_left
+ft_write_headshape('earflap_left.stl', earflap_left, 'format', 'stl');
+
+load earflap_right
+ft_write_headshape('earflap_right.stl', earflap_right, 'format', 'stl');
+
+load earcanal_left
+ft_write_headshape('earcanal_left.stl', earcanal_left, 'format', 'stl');
+
+load earcanal_right
+ft_write_headshape('earcanal_right.stl', earcanal_right, 'format', 'stl');
+
 %% we can plot all geometrical objects together in MATLAB and in principle you could also adjust the position of the vertex cylinder and the binoculars in MATLAB. Since the alignment of these with the CTF coordinate system is not crucial, you can also shift them around in external 3D editing software (see the next step).
 
 figure
-ft_plot_mesh(scalp_coreg3)
+ft_plot_mesh(scalp_coreg3, 'facealpha', 1, 'facecolor', 'skin')
 ft_plot_mesh(headcoil_nas_coreg3, 'edgecolor', 'none', 'facecolor', 'r')
 ft_plot_mesh(headcoil_lpa_coreg3, 'edgecolor', 'none', 'facecolor', 'g')
 ft_plot_mesh(headcoil_rpa_coreg3, 'edgecolor', 'none', 'facecolor', 'g')
 ft_plot_mesh(combined_axes, 'edgecolor', 'none', 'facecolor', 'm')
 ft_plot_mesh(vertex_cylinder, 'edgecolor', 'none', 'facecolor', 'y')
 ft_plot_mesh(binoculars, 'edgecolor', 'none', 'facecolor', 'y')
+ft_plot_mesh(earflap_left, 'edgecolor', 'none', 'facecolor', 'y')
+ft_plot_mesh(earflap_right, 'edgecolor', 'none', 'facecolor', 'y')
+ft_plot_mesh(earcanal_left, 'edgecolor', 'none', 'facecolor', 'y')
+ft_plot_mesh(earcanal_right, 'edgecolor', 'none', 'facecolor', 'y')
+ft_plot_mesh(dewar, 'edgecolor', 'none', 'facecolor', 'darkgreen')
 camlight
 
-%% Third phase: align the scalp with the dewar and combine/crop the model, this is implemented in MeshMixer
+% note that the position of the dewar relative to the scalp will not be optimal yet
 
-% - load the geometrical model of the aligned head surface
-% - load the geometrical model of the LPA, RPA and NAS head localizer coils
+
+%% Third phase: this is implemented in MeshMixer
 
 % The head surface and the localizer coils should not be moved any more, since that 
 % invalidates the coregistration with the anatomical MRI and thereby with the source 
 % models that you will construct from it, e.g., using FreeSurfer.
 
-% - load the geometrical model of the "binoculars"
+% - align the dewar, earflaps, and earcanals with the scalp 
 % - if needed move the binoculars to match the subjects eye position and gaze direction
-% - load the geometrical model of the vertex cylinder
 % - if needed move the vertex cylinder
-% - merge the head surface, coils, cylinder and binoculars in a single surface
-% - load the model of the dewar
-% - move the dewar so that the head is properly positioned, paying attention to the gaze direction
-% - crop the aggregate head model to remove everything that is outside the dewar
-% - crop the aggregate head model to remove the neck and lower head
-
-%% Fourth phase: optimize the STL model for 3D printing, this is implemented in MeshMixer
-
+% - combine/crop the model, this is implemented in MeshMixer
 % - make the aggregate head model hollow
 % - export the aggregate head model to an STL file to get it printed
